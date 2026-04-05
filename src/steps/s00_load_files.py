d68f0659dc06dee2db744341a4f73f9340704a36
marlon-dk-cooh <marlon.marin@dataknow.co>
2026-04-05 14:48:20 -0500

    debug: test de funcionamiento de nuevas rutas dbfs y generacion de metadata para paso s00

--- src/steps/s00_load_files.py
+++ src/steps/s00_load_files.py
@@ -37,40 +37,40 @@ def main(container: str, medallion: str, directory: str, **kwargs):
 
     return paths
 
-    # # 3. Procesar cada archivo con classify_file_by_extension y obtener el tamaño.
-    # file_sizes = {}
-    # for file_path in paths:
-    #     group, ext = classify_file_by_extension(file_path)
-    #     size_file = storage.get_file_size(directory=file_path)
-    #     file_sizes[file_path] = f"{size_file} kB"
-    #     logger.info(f"\n--- Procesando: {file_path} [Grupo: {group}, Extensión: {ext}, Tamaño: {size_file} kB] ---")
+    # 3. Procesar cada archivo con classify_file_by_extension y obtener el tamaño.
+    file_sizes = {}
+    for file_path in paths:
+        group, ext = classify_file_by_extension(file_path)
+        size_file = storage.get_file_size(directory=file_path)
+        file_sizes[file_path] = f"{size_file} kB"
+        logger.info(f"\n--- Procesando: {file_path} [Grupo: {group}, Extensión: {ext}, Tamaño: {size_file} kB] ---")
     
-    # # Archivos clasificados por extensión
-    # grouped_paths = group_files_by_extension(paths) 
+    # Archivos clasificados por extensión
+    grouped_paths = group_files_by_extension(paths) 
     
-    # # Porcentaje de archivos por grupo
-    # percent_group = proportion_by_file_group(paths)
+    # Porcentaje de archivos por grupo
+    percent_group = proportion_by_file_group(paths)
 
-    # # Extensiones en la categoria de "structured", segun la clasificacion en FILE_GROUPS.
-    # ext_struc_paths = ext_in_structured(grouped_paths)
+    # Extensiones en la categoria de "structured", segun la clasificacion en FILE_GROUPS.
+    ext_struc_paths = ext_in_structured(grouped_paths)
 
-    # # Extensiones en la categoria de "others", segun la clasificacion en FILE_GROUPS.
-    # ext_other_paths = ext_in_others(grouped_paths)
+    # Extensiones en la categoria de "others", segun la clasificacion en FILE_GROUPS.
+    ext_other_paths = ext_in_others(grouped_paths)
 
-    # # Escribir archivos con los resultados (por ahora en local).
-    # os.makedirs(kwargs["output_path"], exist_ok=True)
+    # Escribir archivos con los resultados (por ahora en local).
+    os.makedirs(kwargs["output_path"], exist_ok=True)
 
-    # if os.path.exists(kwargs["output_path"]):
-    #     with open(os.path.join(kwargs["output_path"], kwargs["extension_in_others_class"]), "w") as f:
-    #         f.write(json.dumps(ext_other_paths, indent=2))
-    #     with open(os.path.join(kwargs["output_path"], kwargs["len_per_group_extension"]), "w") as f:
-    #         f.write(json.dumps(percent_group, indent=2))
-    #     with open(os.path.join(kwargs["output_path"], kwargs["grouped_extension"]), "w") as f:
-    #         f.write(json.dumps(grouped_paths, indent=4))
-    #     with open(os.path.join(kwargs["output_path"], kwargs["extension_in_structured_class"]), "w") as f:
-    #         f.write(json.dumps(ext_struc_paths, indent=2))
-    #     with open(os.path.join(kwargs["output_path"], kwargs["file_sizes"]), "w") as f:
-    #         f.write(json.dumps(file_sizes, indent=2))
+    if os.path.exists(kwargs["output_path"]):
+        with open(os.path.join(kwargs["output_path"], kwargs["extension_in_others_class"]), "w") as f:
+            f.write(json.dumps(ext_other_paths, indent=2))
+        with open(os.path.join(kwargs["output_path"], kwargs["len_per_group_extension"]), "w") as f:
+            f.write(json.dumps(percent_group, indent=2))
+        with open(os.path.join(kwargs["output_path"], kwargs["grouped_extension"]), "w") as f:
+            f.write(json.dumps(grouped_paths, indent=4))
+        with open(os.path.join(kwargs["output_path"], kwargs["extension_in_structured_class"]), "w") as f:
+            f.write(json.dumps(ext_struc_paths, indent=2))
+        with open(os.path.join(kwargs["output_path"], kwargs["file_sizes"]), "w") as f:
+            f.write(json.dumps(file_sizes, indent=2))
 
 if __name__ == "__main__":
     # Logs
@@ -80,14 +80,11 @@ if __name__ == "__main__":
             container=CONTAINER,
             medallion=MEDALLION,
             directory="",
-            # output_path="./helpers", 
-            # extension_in_others_class="others_ext.json", 
-            # len_per_group_extension="len_per_group.json", 
-            # grouped_extension="grouped_ext.json",
-            # extension_in_structured_class="structured_ext.json",
-            # file_sizes="file_sizes.json"
+            output_path="./helpers", 
+            extension_in_others_class="others_ext.json", 
+            len_per_group_extension="len_per_group.json", 
+            grouped_extension="grouped_ext.json",
+            extension_in_structured_class="structured_ext.json",
+            file_sizes="file_sizes.json"
     )
-
-    for path in paths:
-        print(f"Ruta: {path}")
 
